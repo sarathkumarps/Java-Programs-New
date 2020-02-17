4ca1c1bc43545823f245e5e31d227d45bfedeb2c
quest-pc <quest-pc@192.168.0.80>
2020-02-17 16:21:11 +0530

    Deserialization

---
+++ New Project/src/serializationPgms/Deserializationex.java
@@ -0,0 +1,28 @@
+package serializationPgms;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
+public class Deserializationex {
+	public static void main(String[] args) {
+		//Creating Stream to read Object
+		
+		
+		try {
+			FileInputStream input = new FileInputStream("D:\\serialisation.txt");
+			ObjectInputStream in=new ObjectInputStream(input);
+			Student s=(Student)in.readObject();
+			System.out.println(s.id+" "+s.name+" "+s.age+" ");
+			in.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+		}
+		
+		
+	}
+
+}
